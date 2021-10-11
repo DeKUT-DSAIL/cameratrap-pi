1258308222e026e989e2a3c7e3847fe47bc1da9d
Lorna Mugambi <74656615+lornamugambi@users.noreply.github.com>
2021-10-11 23:04:03 +0300

    Update power-management-bash.sh

--- power-management-bash.sh
+++ power-management-bash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-source power-env/bin/activate
+source /home/pi/powering-raspberrypi/power-env/bin/activate
 
 python /home/pi/powering-raspberrypi/power-management.py -w "((6,11),(15,19))"
 
