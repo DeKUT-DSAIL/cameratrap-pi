900396a4cb859865e62ccc1696d5f51eadd9f8f0
Yuri-Njathi <54037190+Yuri-Njathi@users.noreply.github.com>
2023-09-01 17:11:07 +0300

    Update power.sh

--- power.sh
+++ power.sh
@@ -2,5 +2,5 @@
 
 source /home/pi/powering-raspberrypi/power-env/bin/activate
 
-python /home/pi/powering-raspberrypi/power.py -w "((6,11),(15,19))"
+python /home/pi/powering-raspberrypi/power.py -w "((6,9),(17,19))"
 
